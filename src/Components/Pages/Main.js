a9724929a7c24cc87a60db8d0cd52f6877bb9dde
songseungeun <grayandjj@gmail.com>
2020-05-25 09:45:34 +0900

    add Main.js header, maincontents

--- src/Components/Pages/Main.js
+++ src/Components/Pages/Main.js
@@ -4,17 +4,17 @@ import './Main.css';
 // import Map from '../Templates/Maps/Map';
 // import Test from '../Templates/Test/Test'; // 테스트용 컴포넌트(삭제예정)
 // import { MapContext } from '../../ContextAPI/MapContext';
-// import MainContents from './MainContents';
-// import Header from './Header';
+import MainContents from '../Templates/MainContents/MainContents';
+import Header from '../Templates/Header/Header';
 
 function Main() {
   // const { state } = useContext(MapContext);
   return (
     <main className="Main">
       {/* <Test /> */}
-      {/* <Header /> */}
+      <Header />
       {/* <Map /> */}
-      {/* <MainContents /> */}
+      <MainContents />
     </main>
   );
 }
