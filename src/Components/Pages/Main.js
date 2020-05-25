014511518b4d4900d9d977771c3d0b82e6c9c9b5
angryboo <buyongsong@gmail.com>
2020-05-25 09:13:46 +0900

    add: main css

--- src/Components/Pages/Main.js
+++ src/Components/Pages/Main.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
-// import './Main.css';
+import './Main.css';
 // import Map from '../Templates/Maps/Map';
 // import Test from '../Templates/Test/Test'; // 테스트용 컴포넌트(삭제예정)
 // import { MapContext } from '../../ContextAPI/MapContext';
