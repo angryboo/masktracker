b195a64fd046bd56519801437e7e1a3df707de38
angryboo <buyongsong@gmail.com>
2020-05-25 09:19:23 +0900

    fix: app.js bind context api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import Main from './Components/Pages/Main';
+import { MapProvider } from './ContextAPI/MapContext';
+import { SearchProvider } from './ContextAPI/SearchContext';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <MapProvider>
+        <SearchProvider>
+          <Main />
+        </SearchProvider>
+      </MapProvider>
+    </div>
+  );
 }
 
 export default App;
